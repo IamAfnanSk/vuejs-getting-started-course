748cee2b8a896681b41e6643ca31e5ef742ca44e
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-23 18:05:49 +0530

    chore(createapp): copy changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // HINT: Use this file as your root app file
 
 // HINT 2: You have to create an instance
-// Pass root component to createApp function
-// And mount it to div with id 'app'
+// pass root component to it
+// and mount it to div with id 'app'
