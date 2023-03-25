38c9f9476fc5d6c98d76c9f8a84d7306762f6c56
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-25 18:16:25 +0530

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
-<template></template>
+<template>
+  <button @click="inc">+</button>
+  <button @click="dec">-</button>
 
-<script setup></script>
+  <!-- Add count below -->
+  <p id="ref">{{}}</p>
+
+  <!-- Add computed property below -->
+  <p id="computed">{{}}</p>
+</template>
+
+<script setup>
+// HINT: Use 'ref' function of vue
+
+function inc() {
+  // increment your ref here
+}
+function dec() {
+  // decrement your ref here
+}
+
+// property to use in computed : props.books
+</script>
