b58b4e63a7e88b9ddf82e24b4cbe405f67be5297
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-25 20:40:28 +0530

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,50 @@
-<template></template>
+<template>
+  <div>
+    <h2>Your Shopping Cart</h2>
 
-<script setup></script>
+    <!-- If items array is not empty show this -->
+    <ul v-if="items.length > 0">
+      <!-- add loop to show all items -->
+      <!-- give :key attribute the value of current index -->
+      <li :key=""></li>
+
+      <!-- output should follow this pattern -->
+      <!-- name - price x quantity -->
+    </ul>
+
+    <!-- else show this -->
+    <p>Your cart is empty</p>
+  </div>
+</template>
+
+<script setup>
+import { toRef } from "vue";
+
+// ignore code below
+const props = defineProps({
+  items: {
+    type: Array,
+    required: false,
+    default: [
+      {
+        name: "Product A",
+        price: 9.99,
+        quantity: 2,
+      },
+      {
+        name: "Product B",
+        price: 14.99,
+        quantity: 1,
+      },
+      {
+        name: "Product C",
+        price: 19.99,
+        quantity: 3,
+      },
+    ],
+  },
+});
+// ignore code above
+
+const { items } = toRef(props);
+</script>
