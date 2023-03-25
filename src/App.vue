61950ebc5baa3f14a5399872c15bf721479251e7
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-25 15:40:57 +0530

    chore: updated template

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <!-- Add event listener using 
+    templating syntax for Vue.js 
+    and update ref's value -->
+  <button>Awesome</button>
 
-<script setup></script>
+  <!-- Make content of this h1 dynamic -->
+  <h1></h1>
+
+  <!-- Make content of this p dynamic -->
+  <p></p>
+
+  <!-- NOTE: don't add any other html element in template -->
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// Hint: Create 2 reactive variable using `ref` function
+// and add it to template.
+</script>
